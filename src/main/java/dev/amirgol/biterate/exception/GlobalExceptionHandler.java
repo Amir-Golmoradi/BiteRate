86b7fa6059997b2696ab9001021d022b60d6a895
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-07 23:49:11 +0330

    Add GlobalExceptionHandler for centralized exception handling

---
+++ src/main/java/dev/amirgol/biterate/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package dev.amirgol.biterate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BiteRateException.class)
+    public ResponseEntity<String> handleCustomException(BiteRateException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
