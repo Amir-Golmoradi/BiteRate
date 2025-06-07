8f14a31df9536eb57005a1c36c7c797a3644013b
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-07 23:49:32 +0330

    Add CuisineType enum for supported cuisine types in the RestaurantReview app

---
+++ src/main/java/dev/amirgol/biterate/domain/enums/CuisineType.java
@@ -0,0 +1,15 @@
+package dev.amirgol.biterate.domain.enums;
+
+/**
+ * Enum representing supported cuisine types in the RestaurantReview app.
+ * <p>
+ * These values are used to categorize restaurants and support filtering or search features.
+ * </p>
+ */
+public enum CuisineType {
+    ITALIAN,
+    JAPANESE,
+    PERSIAN,
+    MEXICAN,
+    FRENCH
+}
