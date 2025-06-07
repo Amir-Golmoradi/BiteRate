46e33077fe67ea547bd099d36047c9298c4e1799
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-07 11:23:01 +0330

    Initial commit

---
+++ src/main/java/dev/amirgol/biterate/BiteRateApplication.java
@@ -0,0 +1,13 @@
+package dev.amirgol.biterate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiteRateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/amirgol/biterate/BiteRateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.amirgol.biterate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
